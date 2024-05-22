26638aa79b0787b62a99c1c48889efd91e86ed47
soniharshil54 <sharshil43@yahoo.com>
2024-05-22 20:03:32 +0530

    unit testing with jest added

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['build', 'tests'],
+  moduleFileExtensions: ['js', 'json'],
+  testMatch: ['**/tests/**/*.test.js'],
+};
---
+++ tests/Checkout.test.js
@@ -0,0 +1,29 @@
+const { Checkout } = require('../build/services/Checkout');
+const { pricingRules } = require('../build/services/pricingRulesConfig');
+
+describe('Checkout', () => {
+  let co;
+
+  beforeEach(() => {
+    co = new Checkout(pricingRules);
+  });
+
+  test('Total expected: $249.00', () => {
+    co.scan('atv');
+    co.scan('atv');
+    co.scan('atv');
+    co.scan('vga');
+    expect(co.total()).toBe(249.00);
+  });
+
+  test('Total expected: $2718.95', () => {
+    co.scan('atv');
+    co.scan('ipd');
+    co.scan('ipd');
+    co.scan('atv');
+    co.scan('ipd');
+    co.scan('ipd');
+    co.scan('ipd');
+    expect(co.total()).toBe(2718.95);
+  });
+});
